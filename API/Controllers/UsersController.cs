c7410ce53e8931a99551761e09f121d224e1c004
DimitarAtanassov <dimitar789558@gmail.com>
2024-07-15 12:56:29 -0700

    Api Skeleton

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+[ApiController]
+[Route("api/[controller]")] //api/users ([controller] is replaced with the first part of our Controller class name so Users)
+
+//Primary constructor syntax
+public class UsersController(DataContext context) : ControllerBase
+{
+    
+
+    // This Creats an HttpGet EndPoint, then we create a method that we will use to return http response to client (We are returning an ActionResult from our Api Endpoint that contains a collection)
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        // Gets List of users from Database
+        var users = await context.Users.ToListAsync();
+        
+        return users;   // Because we use ActionResult this will return an HTTP ok response (200) for return type of IEnumerable<AppUser>
+    }
+
+    [HttpGet("{id:int}")]   // /api/users/3  Adding a route parameter id of type int
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        
+        var user = await context.Users.FindAsync(id);
+        
+        if (user == null) return NotFound(); // 404 Not Found
+        return user; 
+    }
+
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Entities;
+
+/*
+    With Entity Framework, the class define the table and the Properties are the column of the table
+    The table name is defined in DataContext file (public DbSet<AppUser> Users { get; set; }) So our AppUser table will be name Users
+*/
+public class AppUser
+{
+
+    public int Id { get; set; }
+    public required string UserName { get; set; }
+}
